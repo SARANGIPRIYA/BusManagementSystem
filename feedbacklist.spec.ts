bae5093c4ee1fc681edb57d4c653004e1d85755e
SARANGIPRIYA <119725310+SARANGIPRIYA@users.noreply.github.com>
2024-01-08 11:08:17 +0530

    Add files via upload

---
+++ feedbacklist.spec.ts
@@ -0,0 +1,7 @@
+import { Feedbacklist } from './feedbacklist';
+
+describe('Feedbacklist', () => {
+  it('should create an instance', () => {
+    expect(new Feedbacklist()).toBeTruthy();
+  });
+});
